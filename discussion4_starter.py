c684c49d9eba95362ac1df9afeaa9858822c4fc7
Brady Teichman <bradyteichman@Bradys-MacBook-Air-3.local>
2023-09-20 12:48:48 -0400

    Finsihed

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -1,50 +1,29 @@
 class Rectangle():
-    # Create the constructor "__init__" method
-    # Arguments: width (an ingeter), height (an integer)
-    # 
-    # It sets an instance variable, "width" to the passed argument, width
-    # It sets an instance variable, "height" to the passed argument, height
 
-    # YOUR CODE HERE
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-
-
-    # Create the "__str__" method
-    #
-    # It returns a string, 
-    #       "A rectangle with width ____ and height ____"
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "verify_input" method
-    #
-    # It returns a boolean
-    #       True if the width and height are positive numbers
-    #       False otherwise
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "area" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the area of the rectangle.
-
-    # YOUR CODE HERE
-
-
-
-    # Create the "perimeter" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the perimeter of the rectangle.
-
-    # YOUR CODE HERE
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
     
+    def verify_input(self):
+        if self.width > 0 and self.height > 0:
+            return True
+        else:
+            return False
+    
+    def area(self):
+        if self.width > 0 and self.height > 0:
+            return self.width * self.height
+        else:
+            return 'Invalid input'
 
+    def perimeter(self):
+        if self.width > 0 and self.height > 0:
+            return 2 * self.width + 2 * self.height
+        else:
+            return 'Invalid input'    
 
 def main():
     r = Rectangle(10, 10)
@@ -60,4 +39,4 @@ def main():
     print()
 
 if __name__ == "__main__":
-    main()
+    main() 
